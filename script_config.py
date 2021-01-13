499e91fe59a03540020e4cdbd84ddb1d29b8dee4
theolemague <theo.lm@hotmail.fr>
2021-01-13 11:14:36 +0100

    OSPF MPLS

--- script_config.py
+++ script_config.py
@@ -1,23 +1,44 @@
 import json
 import telnetlib
 
-preload_str= "!\n\n!\nversion 12.4\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\nno service password-encryption\n!\n"
-end_str = "control-plane\n!\n!\n!\n!\nline con 0\n exec-timeout 0 0\n logging synchronous\nline aux 0\nline vty 0 4\n login!\n!\nend\n"
-
-
 def config_router(port, router):
 
     tn = telnetlib.Telnet("localhost", port)
-    tn.write(b'en\r\n')
-    tn.write(b'conf t\r\n')
+    tn.write(b'\ren\r')
+
+    tn.write(b'conf t\r')
+    tn.write(bytes("hostname "+router["name"]+"\r",'utf-8'))
+    
+    # (conf-if) inter
     for inter in router["interfaces"]:
-        print(inter)
-        tn.write(bytes("interface "+inter["name"]+"\r\n",'utf-8'))
-        tn.write(bytes("ip address "+inter["address"]+" "+inter["mask"]+"\r\n", "utf-8"))
-        tn.write(b"no shutdown\r\n")
-        tn.write(b"exit\r\n")
-    tn.write(b'do write\r\n')
-    print(tn.read_very_eager().decode("ascii"))
+        tn.write(bytes("interface "+inter["name"]+"\r",'utf-8'))
+        tn.write(bytes("ip address "+inter["address"]+" "+inter["mask"]+"\r", "utf-8"))
+        tn.write(b"no shutdown\r")
+        if inter["name"] != "Loopback0" and inter["mpls"]:
+            tn.write(b"mpls ip\r") 
+            tn.write(bytes("mpls mtu "+router["mpls"]["mtu"]+"\r",'utf-8'))
+        tn.write(b"exit\r")
+    
+    #(conf) OSPF
+    if "ospf" in router:
+        ospf = router["ospf"]
+        tn.write(bytes("router ospf "+ospf["id"]+"\r",'utf-8'))
+        for network in ospf["networks"]:
+        # for inter in router["interfaces"]:
+            tn.write(bytes("network "+network["address"]+" "+network["mask"]+" area "+network["area"]+"\r",'utf-8'))
+
+    # (conf) MPLS
+    if router["mpls"]:
+        mpls = router["mpls"]
+        tn.write(b"ip cef\r")
+        tn.write(b"mpls ldp router-id Loopback0\r")
+        if "min_label" in mpls:
+            tn.write(bytes("mpls label range "+mpls["min_label"]+" "+mpls["max_label"]+"\r",'utf-8'))
+
+    
+    tn.write(b"end\r")
+    tn.write(b'write\r')
+    print("Done for", router["name"])
 
 
 if __name__ == "__main__":
